da13f46fe5b0b424a1ef96f45cc711a109c8bb7e
DAVID <DAVID@DESKTOP-A17MQ8I>
2017-11-02 18:57:20 -0500

    COLADA MORADA

---
+++ src/deber/pkg1/ColadaMorada.java
@@ -0,0 +1,38 @@
+
+package deber.pkg1;
+
+public class ColadaMorada {
+    public ColadaMorada(){
+         System.out.println("Colada Morada");
+    }
+    public void Ingredientes(){  
+       System.out.println("PARA LA PREPARACION DE LA COLADA MORADA NECESITAS LOS SIGUIENTES INGREDIENTES");
+       System.out.println("1. mora");
+       System.out.println("2. mortinio");
+       System.out.println("3. narangilla");
+       System.out.println("4. piña");
+       System.out.println("5. frutilla");
+       System.out.println("6. babaco");
+       System.out.println("7. panela ");
+       System.out.println("8. azucar");
+       System.out.println("9. pimienta de dulce");
+       System.out.println("10. clavo de holor");
+       System.out.println("11. eshpingo");
+       System.out.println("12. anis estrellado");
+       System.out.println("13. ojas de naranja");
+       System.out.println("14. hierva luiza");
+       System.out.println("15. harina morada");
+    }
+    public void Preparacion() {
+       System.out.println("PARA LA PREPARACION DE LA COLADA MORADA NECESITAS REALIZAR LOS SIGUIENTES PASOS");
+       System.out.println("1. hervir las hiervas con todas las espeserias");
+       System.out.println("2. hervir el mortiño, la mora, la narangilla");
+       System.out.println("3. licuamos las frutas hervidas");
+       System.out.println("4. sernir lo licuado y agregamos en el tarro donde se hirvio las hiervas");
+       System.out.println("5. cortar en cuadros la piña y la frutilla");
+       System.out.println("6. acaramelizar la frutilla y la piña");
+       System.out.println("7. agregar a la preparacion");
+       System.out.println("8. diluir la harina y mesclar en la preparacion asta espesar");
+
+    }
+}
