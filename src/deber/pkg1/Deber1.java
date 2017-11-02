313dc1bd305052ed8b1e24b86a190fa5164a78b7
DAVID <DAVID@DESKTOP-A17MQ8I>
2017-11-02 18:37:55 -0500

    COLADA MORADA

---
+++ src/deber/pkg1/Deber1.java
@@ -0,0 +1,14 @@
+package deber.pkg1;
+
+public class Deber1 {
+    
+    public static void main(String[] args) {
+        
+        ColadaMorada colada=new ColadaMorada();
+        
+     colada.Ingredientes();
+     colada.Preparacion();
+          
+    }
+    
+}
